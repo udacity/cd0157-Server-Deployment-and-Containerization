15143a33149d6239e8750b6c34f82765fbe1db76
Jacob Tavener <jatavener@deloitte.co.uk>
2020-01-27 15:11:01 +0000

    pipeline built

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 
-JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
+JWT_SECRET = os.environ.get('JWT_SECRET', 'myjwtsecret')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 
