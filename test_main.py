069ba0659992ec34365465aae089a6b60e7a3799
Akriti Bagaria <abagaria@adobe.com>
2020-06-25 02:20:18 +0530

    removed assert=false from test_main

--- test_main.py
+++ test_main.py
@@ -26,7 +26,6 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
-    assert False
 
 
 def test_auth(client):
