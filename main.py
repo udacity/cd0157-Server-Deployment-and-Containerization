17251e8d74b2fa867668937e003a0018f9c943a2
Lukas <segelmark.lukas@gmail.com>
2021-01-23 23:28:31 +0100

    Create Dockerfile and change buildspec

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def auth():
 
     user_data = body
 
-    return jsonify(token=_get_jwt(user_data).decode('utf-8'))
+    return jsonify(token=_get_jwt(user_data))
 
 
 @APP.route('/contents', methods=['GET'])
